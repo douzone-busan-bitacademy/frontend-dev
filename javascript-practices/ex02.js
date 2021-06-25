535633f35652c640a377884bd54295d3e20c50c1
douzone-busan-bitacademy <douzone.busan.bitacademy@gmail.com>
2021-06-25 16:27:31 +0900

    객체 #2

--- javascript-practices/ex02.js
+++ javascript-practices/ex02.js
@@ -20,6 +20,14 @@
  
 
 [객체2] - function type 
+1. new 생성자함수() 사용해서 생성,
+   Function() -> function type 객체가 생성
+2. funtion(....) { ..... } ( 함수 리터럴)
+*/
+
+/*
+
+
 
 */
 
@@ -40,6 +48,7 @@ var b2 = new Boolean(true);
 var s2 = new String("Hello World");
 var o = new Object(true);
 var a = new Array();
+
 var n = null;
 var a2 = [];
 var o2 = {};
@@ -47,15 +56,27 @@ var o2 = {};
 console.log("i2 :" + typeof(i2));
 console.log("b2 :" + typeof(b2));
 console.log("s2 :" + typeof(s2));
-console.log("o2 :" + typeof(o));
-console.log("a2 :" + typeof(a));
-console.log("o2 :" + typeof(n));
+console.log("o :" + typeof(o));
+console.log("a :" + typeof(a));
+console.log("n :" + typeof(n));
 console.log("a2 :" + typeof(a2));
-console.log("a2 :" + typeof(o2));
+console.log("o2 :" + typeof(o2));
 
 
 console.log("====[객체2]: function type =====================")
+var f = new Function("a", "b", "return a + b;");
+var f2 = function(a, b) { return a + b; }
 
-var n = null;
+console.log("f: " + typeof(f));
+console.log("f2: " + typeof(f2));
 
 
+console.log("====원시타입과 원시타입을 Wrapper 객체는 구분없이 사용가능하다. =====================")
+console.log(i + i2);
+console.log(s + s2);
+
+// 원시 타입에 메서드가 호출될 때,
+// 임시 Wrapper객체가 만들어 져서 메서드 호출이 일어난 뒤 사라진다. 
+console.log("====원시타입도 메소드 호출이 가능하다. =====================")
+console.log(b.valueOf());
+console.log(b2.valueOf());
