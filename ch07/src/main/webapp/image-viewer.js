2210ba30dc4751f0d3f3e425317926370b387cb6
douzone-busan-bitacademy <douzone.busan.bitacademy@gmail.com>
2021-07-05 15:07:24 +0900

    imageViewer #3

--- ch07/src/main/webapp/image-viewer.js
+++ ch07/src/main/webapp/image-viewer.js
@@ -3,13 +3,44 @@ var imageViewer = {
 		var that = this;
 		$(function(){
 			$("#btn-change").click(that._changeImage.bind(that));
+			$("#btn-sildeshow").click(that._slideShow.bind(that));
+			$(".image-viewer img").dblclick(that._consoleLog);
+			
+			// 첫 번째 이미지 선택
 			that._changeImage();
 		});
 	},
-	_intervalId: null,
+	_consoleLog: function(){
+		var name = $(this).attr("alt");
+		console.log(name);
+	},
+	_slideShow: function(){
+		if(this._intervalId){
+			/* 슬라이드쇼가 진행 상태 */
+			
+			// 1. 타이머 중지
+			clearInterval(this._intervalId);
+			this._intervalId = null;
+			
+			// 2. 버튼텍스트 => 슬라이드쇼 시작
+			$("#btn-sildeshow").text("슬라이드쇼 시작");
+		} else {
+			/* 슬라이드쇼가 중지 상태 */
+			
+			// 1. 타이머 시작
+			var that = this;
+			that._intervalId = setInterval(function(){
+				that._changeImage();
+			}, 1000);
+			// that._intervalId = setInterval(that._changeImage.bind(that), 1000);
+			
+			// 2. 버튼텍스트 => 슬라이드쇼 중지 
+			$("#btn-sildeshow").text("슬라이드쇼 중지");
+		}
+	},
 	_changeImage: function(){
-		var result = Math.floor(Math.random() * this._images.length);
-		var info = this._images[result];
+		var index = Math.floor(Math.random() * this._images.length);
+		var info = this._images[index];
 		
 		$(".image-viewer img").attr({
 			src: "images/" + info.file,
@@ -17,6 +48,7 @@ var imageViewer = {
 			title: info.name
 		});		
 	},
+	_intervalId: null,
 	_images: [{ 
 			name: "국화",
 			file: "Chrysanthemum.jpg"
